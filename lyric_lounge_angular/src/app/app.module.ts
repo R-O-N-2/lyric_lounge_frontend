15442af7e556ba443034b17f77042f611fffd1b3
Ron <ron_rose@me.com>
2023-08-01 16:49:43 -0400

    got components now

--- lyric_lounge_angular/src/app/app.module.ts
+++ lyric_lounge_angular/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { WorksComponent } from './components/works/works.component';
+import { WorksListComponent } from './components/works/works-list/works-list.component';
+import { WorkCreateComponent } from './components/works/work-create/work-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WelcomeComponent,
+    LoginComponent,
+    SignupComponent,
+    WorksComponent,
+    WorksListComponent,
+    WorkCreateComponent
   ],
   imports: [
     BrowserModule,
---
+++ lyric_lounge_angular/src/app/components/works/work-create/work-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-create',
+  templateUrl: './work-create.component.html',
+  styleUrls: ['./work-create.component.scss']
+})
+export class WorkCreateComponent {
+
+}
